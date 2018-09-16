552153215a82d91872101ecaf2c68fd5d8887484
alex_mac <alexey.rozhin90@gmail.com>
2018-09-16 13:56:52 +0300

    create cite post request

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 const express = require("express");
 const formidable = require("express-formidable");
+const bodyParser = require('body-parser');
 const router = require("./routes/index");
-const bodyParser = require("body-parser");
 
 const app = express();
-app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
-app.use(
-  formidable({
-    uploadDir: "uploads/imgs",
-    multiples: false
-  })
-);
+app.use(express.static('public'));
+
+// app.use(
+//   formidable({
+//     uploadDir: "uploads/imgs",
+//     multiples: false
+//   })
+// );
 
 app.use("/cites", router);
 
